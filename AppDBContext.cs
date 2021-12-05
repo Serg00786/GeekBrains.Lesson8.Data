96d41e496416a8cd63650d24c2335513be455399
Serg <a_s_v86@mail.ru>
2021-12-05 22:46:21 +0300

    Initial

---
+++ AppDBContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace GeekBrains.Lesson8.Data
+{
+
+    public class StudentsContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public StudentsContext()
+        {
+
+        }
+
+        public StudentsContext(DbContextOptions<StudentsContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ListOfStudent> ListOfStudent{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Students;User ID=SA;Password=1qaz!QAZ;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+    }
+
+
+}
---
+++ Repository.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains.Lesson8.Data
+{
+    public interface IRepository
+    {
+        Task<List<ListOfStudentDTO>> GetStudents();
+    }
+
+    public class Repository : IRepository
+    {
+        public async Task<List<ListOfStudentDTO>> GetStudents()
+        {
+            var db = new StudentsContext();
+            var Result = await db.ListOfStudent.ToListAsync();
+            var ResultMapped = Mapping.Mapper.Map<List<ListOfStudentDTO>>(Result);
+            return ResultMapped;
+        }
+    }
+}
